f366e6a9922fcdbb4ba8e39b433ba01bf694f992
AllisonMcDonnell <allison.l.mcdonnell@gmail.com>
2015-03-24 17:49:40 -0500

    changed Rails version

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+RAILS_GEM_VERSION = '2.2.0' unless defined? RAILS_GEM_VERSION
+
 # Initialize the Rails application.
 Rails.application.initialize!
